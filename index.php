f3dd3bf19250c7937a718454010b0882292cabeb
Dolgov Dmitriy <TwilightTower@mail.ru>
2017-06-17 18:52:20 +0300

    refactor; ruble sign

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
@@ -13,6 +14,7 @@ $items = require 'product_list.php';
     <meta charset="UTF-8">
     <title>Продажа цветов</title>
     <link rel="stylesheet" href="jquery-ui-1.12.1/jquery-ui.css">
+    <link rel="stylesheet" href="css/products.css">
     <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="jquery-ui-1.12.1/jquery-ui.min.js"></script>
 
@@ -47,27 +49,30 @@ $items = require 'product_list.php';
 </head>
 <body>
 
-<ul id="source" class="connectedSortable">
+<div id="products">
 
-    <?php foreach ($items as $elem): ?>
+    <ul id="source" class="connectedSortable">
 
-        <li class="ui-state-default">
-            <a class="item-image" data-id="<?php echo sc($elem['id']) ?>">
-                <img src="/images/<?php sc($elem['image']) ?>" style="width: 69px; height: 64px;"
-                     alt="<?php sc($elem['name']) ?>"></a>
+        <?php foreach ($items as $elem): ?>
 
-            <div class="item-info"><?php sc($elem['name']) ?>
-                <div class="price-value"><?php sc($elem['price']) ?>
-                    <span style="font-family:u2000">₽</span>
+            <li class="ui-state-default" data-id="<?php echo sc($elem['id']) ?>">
+                <img class="product_image" src="images/<?php echo sc($elem['image']) ?>"
+                     alt="<?php echo sc($elem['name']) ?>">
+
+                <div class="item-info">
+                    <div class="product_name"><?php echo sc($elem['name']) ?></div>
+                    <div class="product_price"><?php echo sc($elem['price']) ?></div>
+                    <img src="ruble_sign.png" style="width: 79px; height: 100px;">
                 </div>
-            </div>
-        </li>
+            </li>
 
-    <?php endforeach; ?>
+        <?php endforeach; ?>
 
-</ul>
+    </ul>
 
-<ul id="destination" class="connectedSortable"></ul>
+    <ul id="destination" class="connectedSortable" style="display:none"></ul>
+
+</div>
 
 </body>
 </html>
