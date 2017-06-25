31c6974e2bbfbf8fa0774182854ec1428e16a821
Dolgov Dmitriy <TwilightTower@mail.ru>
2017-06-26 00:18:02 +0300

    flower positioning

--- js/products.js
+++ js/products.js
@@ -115,18 +115,28 @@ jQuery(function ($) {
         return array;
     }
 
+    function getTotalFlowersCount() {
+        var totalFlowerCount = 0;
+        $(".products .product_amount").each(function () {
+            totalFlowerCount += parseInt($(this).val());
+        });
+        console.log("totalFlowerCount: " + totalFlowerCount);
+        return totalFlowerCount;
+    }
 
     function drawBoquet(flowers, totalAmount) {
         var elements = [];
-        var sizes = calculateBouquetSizes(totalAmount);
+        var flowersAmount = getTotalFlowersCount();
+        var sizes = calculateBouquetSizes(flowersAmount);
         for (var fl in flowers) {
             for (var i = 0; i < flowers[fl].amount; ++i) {
-                var pos = getRandomPosition(sizes.bouquetDiameter / 2);
+                var width = Math.floor((Math.random() * (sizes.maxFlowerDiameter - sizes.minFlowerDiameter)) + sizes.minFlowerDiameter);
+                var pos = getRandomPosition(sizes.bouquetDiameter / 2, width, flowersAmount);
                 elements.push({
                     x: pos.x,
                     y: pos.y,
-                    width: Math.floor((Math.random() * (sizes.maxFlowerDiameter - sizes.minFlowerDiameter)) + sizes.minFlowerDiameter),
-                    id: fl
+                    width: width,
+                    id: fl,
                 });
                 /*raiseFlower(pos.x, pos.y,
                  Math.floor((Math.random() * (sizes.maxFlowerDiameter - sizes.minFlowerDiameter)) + sizes.minFlowerDiameter), fl);*/
@@ -139,11 +149,63 @@ jQuery(function ($) {
         }
     }
 
-    function getRandomPosition(radius) {
+    function getRandomPosition(radius, width, flowersAmount) {
+        /*var pt_angle = Math.random() * 2 * Math.PI;
+         var pt_radius_sq = Math.random() * radius * radius;
+         var pt_x = Math.sqrt(pt_radius_sq) * Math.cos(pt_angle);
+         var pt_y = Math.sqrt(pt_radius_sq) * Math.sin(pt_angle);*/
+
+        /*var halfX = $(".destination").width() / 2;
+        var halfY = $(".destination").height() / 2;
+
+        var halfObject = width / 2;
+
+        console.log("width: " + width + "; flowersAmount: " + flowersAmount + "; halfX: " + halfX + "; halfY: " + halfY);
+
+        if (flowersAmount > 30) {
+            flowersAmount = 30;
+        }
+
+        var offsetX = (halfX - halfObject) * (flowersAmount / 30 * Math.random());
+        var offsetY = (halfY - halfObject) * (flowersAmount / 30 * Math.random());
+
+        console.log("offsetX: " + offsetX + "; offsetY: " + offsetY);
+
+        if (Math.random() > .5) {
+            pt_x = halfX - halfObject + offsetX;
+        } else {
+            pt_x = halfX - halfObject - offsetX;
+        }
+        if (Math.random() > .5) {
+            pt_y = halfY - halfObject + offsetY;
+        } else {
+            pt_y = halfY - halfObject - offsetY;
+        }*/
+
+        //var pt_x = ($(".destination").width() / 2) + Math.random() * flowersAmount * width;
+        //var pt_y = ($(".destination").height() / 2) + Math.random() * flowersAmount * width;
+
+        /*if (flowersAmount > 50) {
+            flowersAmount = 50;
+        }*/
+
         var pt_angle = Math.random() * 2 * Math.PI;
-        var pt_radius_sq = Math.random() * radius * radius;
+        var pt_radius_sq = Math.random() * width / (1 / flowersAmount);
+        //var pt_x = Math.sqrt(pt_radius_sq) * Math.cos(pt_angle);
+        //var pt_y = Math.sqrt(pt_radius_sq) * Math.sin(pt_angle);
         var pt_x = Math.sqrt(pt_radius_sq) * Math.cos(pt_angle);
         var pt_y = Math.sqrt(pt_radius_sq) * Math.sin(pt_angle);
+
+        /*if (pt_x > $(".destination").width() - width) {
+            pt_x = $(".destination").width() - width;
+        } else if (pt_x < 0) {
+            pt_x = 0;
+        }
+        if (pt_y > $(".destination").height() - width) {
+            pt_y = $(".destination").height() - width;
+        } else if (pt_y < 0) {
+            pt_y = 0;
+        }*/
 
         return {x: pt_x, y: pt_y};
     }
@@ -167,30 +229,47 @@ jQuery(function ($) {
      }*/
 
     function calculateBouquetSizes(newAmount) {
-        var totalFlowerCount = $(".destination .product_item").length + newAmount;
-        var maxBouqetDiameter = Math.min($(".destination").width(), $(".destination").height()) * 0.8;
+        //var totalFlowerCount = $(".destination .product_item").length + newAmount;
+        var totalFlowerCount = getTotalFlowersCount();
+        var maxBouqetDiameter = Math.min($(".destination").width(), $(".destination").height()) * 0.9;
 
         var sizes = {};
 
-        sizes.bouquetDiameter = maxBouqetDiameter / Math.sqrt(totalFlowerCount);
+        //sizes.bouquetDiameter = maxBouqetDiameter / Math.sqrt(totalFlowerCount);
+        sizes.bouquetDiameter = maxBouqetDiameter;
         sizes.maxFlowerDiameter = sizes.bouquetDiameter * 0.7;
-        sizes.minFlowerDiameter = sizes.maxFlowerDiameter * 0.6;
+        sizes.minFlowerDiameter = sizes.maxFlowerDiameter * 0.8;
 
         return sizes;
     }
 
     function raiseFlower(x, y, width, id) {
         console.log("X: " + x + "; Y: " + y + "; Width: " + width + "; id: " + id);
+
+        var halfWidth = width / 2;
+
         x = x + $(".destination").width() / 2;
         y = y + $(".destination").height() / 2;
+
+        if (x > $(".destination").width() - halfWidth) {
+            x = $(".destination").width() - halfWidth;
+        } else if (x < halfWidth) {
+            x = halfWidth;
+        }
+        if (y > $(".destination").height() - halfWidth) {
+            y = $(".destination").height() - halfWidth;
+        } else if (y < halfWidth) {
+            y = halfWidth;
+        }
+
         var src = $(".product[data-id='" + id + "'").data("image_top");
         src = 'images/products/' + encodeURIComponent(src);
         var s = "<img src='" + src + "' style='width:2px;opacity:0.1;left:" + x + "px;top:" + y + "px' class='product_item product_" + id + "'>";
         $(".destination").append(s);
         $(".destination img:last-child").animate({
             width: width + "px",
-            left: (x - width / 2) + "px",
-            top: (y - width / 2) + "px",
+            left: (x - halfWidth) + "px",
+            top: (y - halfWidth) + "px",
             opacity: 1
         }, 400);
         /*var fInterval = setInterval(function () {
